1b22967caf72496b402585f6230a4b6ccd805def
yologger <yologger1013@gmail.com>
2022-09-14 20:32:40 +0900

    [ADD] Spring Data Redis RedisTemplate.opsForList() 테스트 코드 추가

--- 06_spring_redis/01_redis/src/test/java/com/yologger/spring_redis/repository/RedisTemplateTest.java
+++ 06_spring_redis/01_redis/src/test/java/com/yologger/spring_redis/repository/RedisTemplateTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -27,5 +30,15 @@ class RedisTemplateTest {
 
         String saved = operation.get("name");   // "Cristiano Ronaldo"
         assertThat(saved).isEqualTo("Cristiano Ronaldo");
+    }
+
+    @Test
+    @DisplayName("opsForList() 테스트")
+    public void opsForList() {
+        ListOperations<String, String> operations =  redisTemplate.opsForList();
+        operations.rightPush("players", "Ronaldo");
+        operations.rightPushAll("players", "Messi", "Benzema", "Suarez");
+        List<String> players = operations.range("players", 0, -1);  // ["Ronaldo", "Messi", "Benzema", "Suarez"]
+        assertThat(players.size()).isEqualTo(4);
     }
 }
