c3df81ae942e4fb6a68dc23da9584f003c113822
yologger <yologger1013@gmail.com>
2022-09-15 14:42:59 +0900

    [ADD] Spring Data Redis opsForSet() 테스트 코드 작성

--- 06_spring_redis/01_redis/src/test/java/com/yologger/spring_redis/repository/RedisTemplateTest.java
+++ 06_spring_redis/01_redis/src/test/java/com/yologger/spring_redis/repository/RedisTemplateTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
 
 import java.util.List;
@@ -39,6 +40,23 @@ class RedisTemplateTest {
         operations.rightPush("players", "Ronaldo");
         operations.rightPushAll("players", "Messi", "Benzema", "Suarez");
         List<String> players = operations.range("players", 0, -1);  // ["Ronaldo", "Messi", "Benzema", "Suarez"]
+        assertThat(players.size()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("opsForSet() 테스트")
+    public void opsForSet() {
+        SetOperations<String, String> operations =  redisTemplate.opsForSet();
+
+        // Set에 요소 하나 추가
+        operations.add("players", "Paul");
+
+        // Set에 요소 여러 개 추가
+        operations.add("players", "John", "Mike", "Monica");
+
+        // Set 조회
+        Set<String> players =  operations.members("players");
+
         assertThat(players.size()).isEqualTo(4);
     }
 }
