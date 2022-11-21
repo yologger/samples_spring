3fa22a19074fa0112733c42db1596cbb519c53cd
yologger <yologger1013@gmail.com>
2022-11-21 21:47:51 +0900

    [ADD] Prometheus, Grafana 샘플

---
+++ 29_prometheus_grafana/src/main/java/com/yologger/samples/prometheus_grafana/TestController.java
@@ -0,0 +1,18 @@
+package com.yologger.samples.prometheus_grafana;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test1")
+    public String test1() {
+        return "test1";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test2";
+    }
+}
